abe15b8e75a1fe14edc181be6ac2d8f95b4c2d99
Caio Santana <brnoliveirasantana@gmail.com>
2023-12-17 13:32:23 -0300

    Criada store para manipulação de dados com localStorage

---
+++ src/components/add-item.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useItems } from "@/store/items";
+import { FormEvent, useState } from "react"
+
+export function AddItem() {
+    const [item, setItem] = useState("");
+    const { handleAddItem } = useItems();
+
+    function handleFormSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        handleAddItem(item);
+        setItem("");
+    }
+
+    return (
+        <form onSubmit={handleFormSubmit}>
+            <input type="text" placeholder="Escreva o nome do item" value={item} onChange={(e) => setItem(e.target.value)} />
+            <button type="submit">Adicionar</button>
+        </form>
+    )
+}
+
+export default AddItem;
---
+++ src/store/items.tsx
@@ -0,0 +1,81 @@
+'use client'
+import { ReactNode, createContext, useContext, useState } from "react";
+
+export type Item = {
+    id: string;
+    itemName: string;
+    purchased: boolean;
+    createdAt: Date;
+}
+
+export type ItemsContext = {
+    items: Item[];
+    handleAddItem: (item: string) => void;
+    toggleItemAsPurchased: (id: string) => void;
+    handleDeleteItem: (id: string) => void;
+}
+
+export const itemsContext = createContext<ItemsContext | null>(null);
+
+export function ItemsProvider({ children }: { children: ReactNode }) {
+    const [items, setItems] = useState<Item[]>(() => {
+        try {
+            const newItems = localStorage.getItem("items") || "[]";
+            return JSON.parse(newItems) as Item[]
+        } catch (e) {
+            return []
+        }
+    })
+
+    function handleAddItem(itemName: string) {
+        setItems((prev) => {
+            const newItems: Item[] = [
+                {
+                    id: Math.random().toString(),
+                    itemName,
+                    purchased: false,
+                    createdAt: new Date(),
+                },
+                ...prev
+            ];
+            localStorage.setItem("items", JSON.stringify(newItems));
+            return newItems
+        })
+    }
+
+    const toggleItemAsPurchased = (id: string) => {
+        setItems((prev) => {
+            const newItems = prev.map((item) => {
+                if (item.id === id) {
+                    return { ...item, purchased: !item.purchased }
+                }
+                return item;
+            })
+            localStorage.setItem("items", JSON.stringify(newItems));
+            return newItems
+        })
+    }
+
+    function handleDeleteItem(id: string) {
+        setItems((prev) => {
+            const newItems = prev.filter((item) => item.id !== id)
+            localStorage.setItem("items", JSON.stringify(newItems));
+            return newItems
+        })
+    }
+
+    return (
+        <itemsContext.Provider value={{ items, handleAddItem, toggleItemAsPurchased, handleDeleteItem }}>
+            {children}
+        </itemsContext.Provider>
+    )
+}
+
+export function useItems() {
+    const itemsContextValue = useContext(itemsContext);
+    if (!itemsContextValue) {
+        throw new Error('useItems used outside of Provider')
+    }
+
+    return itemsContextValue
+}
